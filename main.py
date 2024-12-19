cd149eb68f3c48f874894bba482999aa16142774
52Alexis <alexis.vilmard@outlook.fr>
2024-12-19 15:51:25 +0800

    output management

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import sys
 from document_parser import NotSupportedFormat, FlashCardParser
-from generator import FlashCardGenerator
+from generator import FlashCardGenerator, FlashCard
 
 def main(argc: int, argv: list):
     if argc < 2:
-        print("insufficient arguments")
+        print("Error : No source file specified.")
         return -1
+    outputfilename = "flashcards.txt"
+    txtoutput = True
+    if argc == 3:
+        if str(argv[2]).split('.')[-1] != "csv" and str(argv[2]).split('.')[-1] != "txt":
+            print("unsupported output format\nThe program will write the output to flashcards.txt")
+        else :
+            outputfilename = str(argv[2])
+            txtoutput = outputfilename.split('.')[-1] == "txt"
+    elif argc == 2:
+        print("he program will write the output to flashcards.txt")
+    if argc > 3:
+        print("Error : Too many arguments")
+        return -1
+
 
     try:
         parser = FlashCardParser(argv[1])
-        content = parser.parse()
+        content: str = parser.parse()
     except NotSupportedFormat as e:
         print(e)
         return -2
@@ -17,14 +31,19 @@ def main(argc: int, argv: list):
         return -2
 
     #content ready to be used
-    flashcards = FlashCardGenerator.generateFlashcard(content)
-    output = ""
-    for flashcard in flashcards:
-        print(flashcard)
-        output += flashcard
-        output += "\n"
+    flashcards: list[FlashCard] = FlashCardGenerator.generateFlashcard(content)
+    if txtoutput:
+        output = ""
+        for flashcard in flashcards:
+            print(str(flashcard))
+            output += str(flashcard)
+            output += "\n"
+    else:
+        output = "Question,Answer\n"
+        for flashcard in flashcards:
+            output += flashcard.getquestion()+','+flashcard.getanswer()+"\n"
 
-    output_file = open("flashcards.txt", "w")
+    output_file = open(outputfilename, "w")
     output_file.write(output)
     output_file.close()
     return 0
