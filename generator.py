604d17f697d2da1d2d468f2db79c9096d7680ef7
52Alexis <alexis.vilmard@outlook.fr>
2024-12-19 15:35:44 +0800

    add class FlashCard

--- generator.py
+++ generator.py
@@ -11,3 +11,36 @@ class FlashCardGenerator:
 
         tokens = []
         return tokens
+
+class FlashCard:
+    def __init__(self,Q:str,A:str):
+        self.__question=Q
+        self.__answer=A
+
+    def flfromlist(QA:list):
+        return FlashCard(QA[0],QA[1])
+
+    def flfromdict(QA:dict):
+        return FlashCard(QA['Question'],QA['Answer'])
+
+    def __str__(self):
+        return "Question : "+self.__question+ "\n\tAnswer: "+self.__answer
+
+    def todict(self):
+        return {"Question":self.__question, "Answer":self.__answer}
+
+    def tolist(self):
+        return [self.__question, self.__answer]
+
+    def setquestion(self,Q:str):
+        self.__question=Q
+
+    def setanswer(self,A:str):
+        self.__answer=A
+
+    def getquestion(self):
+        return self.__question
+
+    def getanswer(self):
+        return self.__answer
+
